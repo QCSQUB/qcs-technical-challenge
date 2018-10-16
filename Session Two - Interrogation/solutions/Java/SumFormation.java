19f41c9e8841c572ff0619d48f6ebbb80e5395d9
Mark O'Hara <mohara20@qub.ac.uk>
2018-10-16 16:50:34 +0100

    bad sum answer

--- Session Two - Interrogation/solutions/Java/SumFormation.java
+++ Session Two - Interrogation/solutions/Java/SumFormation.java
@@ -1,13 +1,25 @@
 class SumFormation{
     public static void main(String []args){
+        final int[] arr = {1,2,4,4};
+        final int sum = 8;
 
+        badSolution(arr, sum);
+        goodSolution(arr, sum);
     }
 
-    public static void badSolution(){
+    public static boolean badSolution(int[] arr, int sum){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j< arr.length; j++){
+                if((arr[i] + arr[j]) == sum && i != j ){
+                    return true;
+                }
+            }
+        }
 
+        return false;
     }
 
-    public static void goodSolution(){
+    public static void goodSolution(int[] arr, int sum){
         
     }
 }
